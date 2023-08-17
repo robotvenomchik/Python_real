0f728d3b1e664f4bffdd68c2624fa5956b0aa019
robotvenomchik <96344010+robotvenomchik@users.noreply.github.com>
2023-08-17 20:13:19 +0300

    cool

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from setting import Setting as s
+DATABASE_URL=f'postgresql+asyncpg://{s.DATABASE_USER}:{s.DATABASE_PASSWORD}@'\
+    f'{s.DATABASE_HOST}:{s.DATABASE_PORT}/{s.DATABASE_HOST}'
+engine=create_async_engine(DATABASE_URL)
+async_session_maker=async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
---
+++ models.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import (
+    Column, Integer, Float, String, DateTime, Boolean,
+)
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String, nullable=False)
+    login = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    is_conflict=Column(Boolean, default=False)
+
