339763c1797f4fb87ae7b7d69c9dc5f747421db8
robotvenomchik <96344010+robotvenomchik@users.noreply.github.com>
2023-08-31 18:53:26 +0300

    fastapi2

--- app/web_pages/router_web_pages.py
+++ app/web_pages/router_web_pages.py
@@ -1,30 +1,48 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from app.menu_data import menu
 
 router = APIRouter(
     prefix='/web',
-    tags=['menu','landing']
+    tags=['menu', 'landing'],
 )
 
-templates=Jinja2Templates(directory='app\\templates')
+templates = Jinja2Templates(directory='app\\templates')
+
+
+# @router.get('/')
+# async def get_main_page(request: Request):
+#     context = {
+#         'request': request,
+#     }
+#
+#     return templates.TemplateResponse(
+#         'base.html',
+#         context=context,
+#     )
+
 
 @router.get('/menu')
-async def get_main_page(request: Request):
-    context={
+async def get_menu(request: Request):
+    context = {
         'request': request,
-
+        'title': 'Наше меню',
     }
+
     return templates.TemplateResponse(
         'menu.html',
-        context=context
+        context=context,
     )
-@router.get('/about-us')
-async def get_main_page(request: Request):
-    context={
-        'request': request,
 
+@router.get('/about-us')
+async def get_menu(request: Request):
+    context = {
+        'request': request,
+        'title': 'Про нас',
+        'menu': menu,
     }
+
     return templates.TemplateResponse(
         'about_us.html',
-        context=context
+        context=context,
     )
